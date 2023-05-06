f4e0af33493a992891761715c07935738d5270e1
Juan Nunez <j.nunezc@uniandes.edu.co>
2023-05-05 23:40:56 -0500

    arreglo

--- monitoring/logIdDev.py
+++ monitoring/logIdDev.py
@@ -42,10 +42,10 @@ def decryptId(position, key):
             unciphered_list.append(descifrado.decode('utf-8'))
         return unciphered_list
 
-ciphered_text = encryptId('123456789', 'doctor', '2020-10-10', '11',  b'QeOzYI2XSk2tAiz1IAcdYnUrEGJzGPbsfwHeXIU4Ecw=')
+#ciphered_text = encryptId('123456789', 'doctor', '2020-10-10', '11')
 #print(ciphered_text)
 
-ciphered_text2 = encryptId('987654321', 'doctor', '2020-10-10', '12',  b'QeOzYI2XSk2tAiz1IAcdYnUrEGJzGPbsfwHeXIU4Ecw=')
+#ciphered_text2 = encryptId('987654321', 'doctor', '2020-10-10', '12')
 #print(ciphered_text2)
 
 unciphered_text = decryptId('11',  b'QeOzYI2XSk2tAiz1IAcdYnUrEGJzGPbsfwHeXIU4Ecw=')
