f9d1f15fbc5ed87675b49683f5ebd06df3166bd9
Juan Nunez <j.nunezc@uniandes.edu.co>
2023-05-05 23:01:18 -0500

    Home historias clinicas

--- historias_clinicas/views.py
+++ historias_clinicas/views.py
@@ -119,7 +119,7 @@ def historia_clinica_create_view(request):
                 historia_clinica = form.save()
                 historia_clinica.save()
                 messages.success(request, 'Historia clinica creada correctamente')
-                return HttpResponseRedirect(reverse('historia_clinica_create'))
+                return render(request, 'historias_clinicas.html')
             else:
                 print(form.errors)
         else:
