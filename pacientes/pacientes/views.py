6ae0bea21f07e37d53f99d5f62c7fc620902c31f
Juan Nunez <j.nunezc@uniandes.edu.co>
2023-05-25 15:23:34 -0500

    Directorios arreglos

--- pacientes/pacientes/views.py
+++ pacientes/pacientes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
-from .pacientes.logic import logic_paciente as lp
+from . import logic as lp
 from django.http import HttpResponse
 from django.core import serializers
 import json
