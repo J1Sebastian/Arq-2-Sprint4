a2e3058c99b7e85353719e9a63a0035e109b134d
Federico Melo Barrero <f.melo@uniandes.edu.co>
2023-05-25 13:12:47 -0500

    ppp a p

--- historiasclinicas/historiasclinicas/views.py
+++ historiasclinicas/historiasclinicas/views.py
@@ -10,7 +10,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render
 import json
-from ..forms import HistoriaClinicaForm
+from forms import HistoriaClinicaForm
 from django.contrib.auth.decorators import login_required
 from autenticador.auth0backend import getRole, getUserId
 
