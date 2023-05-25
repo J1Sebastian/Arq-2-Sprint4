a8b77f847222f047efce8066c3e8ef4786af20b1
Juan Nunez <j.nunezc@uniandes.edu.co>
2023-05-25 18:27:59 -0500

    Arreglo usuario

--- usuarios/usuarios/views.py
+++ usuarios/usuarios/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
-from .usuarios.logic import logic_usuario as la
+from . import logic as la
 from django.http import HttpResponse
 from django.core import serializers
 import json
