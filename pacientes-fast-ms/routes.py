679243021e1409ae0eda33bc3c89d8bfcf0939c0
Federico Melo Barrero <f.melo@uniandes.edu.co>
2023-05-27 23:52:39 -0500

    Y dios creó al hombre del barro

--- pacientes-fast-ms/routes.py
+++ pacientes-fast-ms/routes.py
@@ -14,34 +14,3 @@ def create_paciente(request: Request, paciente: PacienteModel = Body(...)):
         {"_id": new_paciente.inserted_id
     })
     return created_paciente
-
-
-@router.get("/", response_description='Listar pacientes', response_model=List[PacienteModel])
-def list_pacientes(request: Request):
-    pacientes = []
-    for paciente in request.app.database["pacientes"].find():
-        pacientes.append(paciente)
-    return pacientes
-
-
-@router.get("/{id}", response_description='Obtener paciente', response_model=PacienteModel)
-def get_paciente(id: int, request: Request):
-    paciente = request.app.database["pacientes"].find_one({"_id": id})
-    if paciente is not None:
-        return paciente
-    raise HTTPException(status_code=404, detail=f"Paciente not found")
-
-@router.post("/create", response_description='Crear paciente con parametros', status_code=status.HTTP_201_CREATED)
-def create(nombre: str, documento: str, prioridad: str, fecha_nacimiento: str, peso: int, altura: int, tipo_sangre: str, request: Request):
-    # Create a paciente instance (JSON)
-    paciente = {
-        "nombre": nombre,
-        "documento": documento,
-        "prioridad": prioridad,
-        "fecha_nacimiento": fecha_nacimiento,
-        "peso": peso,
-        "altura": altura,
-        "tipo_sangre": tipo_sangre
-    }
-    new_paciente = request.app.database["pacientes"].insert_one(paciente)
-    return request.app.database["pacientes"].find_one({"_id": new_paciente.inserted_id})
