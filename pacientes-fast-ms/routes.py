eab750ddc5de3ac60db52bebe57cf58dced4dcaf
Federico Melo Barrero <f.melo@uniandes.edu.co>
2023-05-27 23:56:26 -0500

    Y creó a la mujer de su costilla xdxd

--- pacientes-fast-ms/routes.py
+++ pacientes-fast-ms/routes.py
@@ -7,10 +7,30 @@ from models import PacienteModel
 router = APIRouter()
 
 @router.post("/", response_description='Crear paciente', status_code=status.HTTP_201_CREATED,response_model=PacienteModel)
-def create_paciente(request: Request, paciente: PacienteModel = Body(...)):
+def post_paciente(request: Request, paciente: PacienteModel = Body(...)):
     paciente = jsonable_encoder(paciente)
     new_paciente = request.app.database["pacientes"].insert_one(paciente)
     created_paciente = request.app.database["pacientes"].find_one(
         {"_id": new_paciente.inserted_id
     })
     return created_paciente
+
+@router.post("/create", response_description='Crear paciente con parametros', status_code=status.HTTP_201_CREATED)
+def create_paciente(nombre: str, documento: str, prioridad: str, fecha_nacimiento: str, peso: int, altura: int, tipo_sangre: str, request: Request):
+    paciente = {
+        "nombre": nombre,
+        "documento": documento,
+        "prioridad": prioridad,
+        "fecha_nacimiento": fecha_nacimiento,
+        "peso": peso,
+        "altura": altura,
+        "tipo_sangre": tipo_sangre
+    }
+    new_paciente = request.app.database["pacientes"].insert_one(paciente)
+    return request.app.database["pacientes"].find_one({"_id": new_paciente.inserted_id})
+
+@router.get("/{id}", response_description='Obtener paciente por id', response_model=PacienteModel)
+def get_paciente(id: str, request: Request):
+    if (paciente := request.app.database["pacientes"].find_one({"_id": id})) is not None:
+        return paciente
+    raise HTTPException(status_code=404, detail=f"Paciente {id} no encontrado")
