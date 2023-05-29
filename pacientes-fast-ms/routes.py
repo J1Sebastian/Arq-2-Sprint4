a964e92438071d581d42407a5f9a29c2f2e8c77f
Mariana Ruiz Giraldo <m.ruizg@uniandes.edu.co>
2023-05-29 16:31:04 -0500

    ygf

--- pacientes-fast-ms/routes.py
+++ pacientes-fast-ms/routes.py
@@ -20,14 +20,17 @@ def create_paciente(nombre: str, documento: str, prioridad: str, fecha_nacimient
         "altura": altura,
         "tipo_sangre": tipo_sangre
     }
-    paciente = request.app.database["pacientes"].insert_one(paciente)
-    return paciente
+    paciente = request.app.database["pacientes"].insert_one(jsonable_encoder(paciente))
+    return request.app.database["pacientes"].find_one({"_id":paciente.inserted_id})
 
 @router.post("/", response_description='Crear paciente', status_code=status.HTTP_201_CREATED,response_model=Paciente)
 def post_paciente_json(request: Request, paciente: Paciente = Body(...)):
     paciente = jsonable_encoder(paciente)
     new_paciente = request.app.database["pacientes"].insert_one(paciente)
-    return new_paciente
+    created_paciente = request.app.database["pacientes"].find_one(
+        {"_id": new_paciente.inserted_id
+    })
+    return created_paciente
 
 
 # Read
