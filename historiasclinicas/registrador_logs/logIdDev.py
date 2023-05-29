af47f63cb7d01782ecf6b7787c5ce205e3cd2b8c
Mariana Ruiz Giraldo <m.ruizg@uniandes.edu.co>
2023-05-29 15:50:17 -0500

    path corregido

--- historiasclinicas/registrador_logs/logIdDev.py
+++ historiasclinicas/registrador_logs/logIdDev.py
@@ -9,7 +9,7 @@ def encryptId(id, role, dateOfCreation, clinicHistoryId):
     textToEncrypt = str(id) + ',' + str(role) + ',' + str(dateOfCreation) + ',' + str(clinicHistoryId)
     cipher_suite = Fernet(b'QeOzYI2XSk2tAiz1IAcdYnUrEGJzGPbsfwHeXIU4Ecw=')
     ciphered_text = cipher_suite.encrypt(textToEncrypt.encode('utf-8'))
-    with open('historiasclinicas/registrador_log/id.json', 'r') as f:
+    with open('./id.json', 'r') as f:
         data = json.load(f)
     with open('./id.json', 'w') as f:
         if clinicHistoryId in data:
